c25bad5817f41a5d9ed8bcc13f8da2e233b2c2f6
2002j <2002jia@duksung.ac.kr>
2023-10-17 00:24:31 +0900

    Week05_1010

---
+++ Week05_LJA/src/project03/BookArrayList.java
@@ -0,0 +1,39 @@
+package project03;
+
+import java.util.ArrayList;
+
+public class BookArrayList {
+	private ArrayList<Book> arrayList; //arrayList선언
+	
+	public BookArrayList() {
+		arrayList = new ArrayList<Book>(); //Book형으로 선언한 ArrayList생성
+	}
+	
+	public void addBook(Book book) { //Book데이터 추가
+		arrayList.add(book);
+	}
+	
+	public boolean removeBook(int BookId) {
+		for(int i = 0; i < arrayList.size();i++) {
+			Book book = arrayList.get(i);
+			int tempId = book.getBookId();
+			if(tempId == BookId) {
+				arrayList.remove(i);
+				return true;
+			}
+		}
+		System.out.println(BookId + "가 존재하지 않습니다.");
+		return false;
+	}
+	
+    public void insertBook(int index, Book book) {
+    	arrayList.add(index, book);
+    }
+    
+	public void showAllBook() {
+		for(Book book : arrayList) {
+			System.out.println(book);
+		}
+		System.out.println();
+	}
+}
