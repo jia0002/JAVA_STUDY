b30aa0ccad93c82649055fbfc8735a58f34ed6cf
2002j <2002jia@duksung.ac.kr>
2023-10-31 06:17:29 +0900

    Week07_1024

---
+++ Week07_LJA/src/project1/Test.java
@@ -0,0 +1,30 @@
+package project1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Test
+{ 
+      
+	public static void main(String[] args) throws IOException {
+		File file1 = new File("test.txt");
+		File file2 = new File("obama.txt"); 
+		char CharCounter = 0;       
+		BufferedReader in = (new BufferedReader(new FileReader(file1)));
+		PrintWriter out = (new PrintWriter(new FileWriter(file2)));
+		int ch;
+		while ((ch = in.read()) != -1)
+		{
+		   if (Character.isLowerCase(ch))  {
+		        ch=Character.toUpperCase(ch);// convert assign variable
+		   }
+		out.write(ch);
+		}
+		in.close();
+		out.close();
+	}
+}
