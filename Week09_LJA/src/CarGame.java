73712064311edc3616a432628ea1ebf4ae9030c3
2002j <2002jia@duksung.ac.kr>
2023-11-07 11:39:50 +0900

    Week09_1107

---
+++ Week09_LJA/src/CarGame.java
@@ -0,0 +1,50 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class CarGame extends JFrame{
+	class MyThread extends Thread{
+		private JLabel label;
+		private int x,y;
+		
+		public MyThread(String fname, int x, int y) {
+			this.x=x;
+			this.y=y;
+			label=new JLabel();
+			label.setIcon(new ImageIcon(fname));
+			label.setBounds(x,y,100,100);
+			add(label);
+		}
+		public void run() {
+			for(int i=0; i<200; i++) {
+				x+=10*Math.random();
+				label.setBounds(x,y,100,100);
+				repaint();
+				try {
+					Thread.sleep(1000);
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public CarGame() {
+		setTitle("CarRace");
+		setSize(600, 300);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);
+		
+		(new MyThread("C:\\Users\\2002j\\eclipse-workspace-z\\Week09_LJA\\src\\car1.gif",100,0)).start();
+		(new MyThread("C:\\Users\\2002j\\eclipse-workspace-z\\Week09_LJA\\src\\car2.gif",100,50)).start();
+		(new MyThread("C:\\Users\\2002j\\eclipse-workspace-z\\Week09_LJA\\src\\car3.gif",100,100)).start();
+		(new MyThread("C:\\Users\\2002j\\eclipse-workspace-z\\Week09_LJA\\src\\car1.gif",100,150)).start();
+		(new MyThread("C:\\Users\\2002j\\eclipse-workspace-z\\Week09_LJA\\src\\car2.gif",100,200)).start();
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		CarGame t = new CarGame();
+	}
+}
---
+++ Week09_LJA/src/LambdaTest.java
@@ -0,0 +1,12 @@
+
+public class LambdaTest {
+
+	public static void main(String[] args) {
+		Runnable task=()->{
+			for(int i=0;i<=10;i++)
+				System.out.print(i+" ");
+		};
+		
+		new Thread(task).start();
+	}
+}
